2bcdcf3b8ed925ff594768ac5937f5b88a9a1c3f
diegao <diegonoguerarec@gmail.com>
2025-06-16 20:17:48 -0300

    Se pueden listar y agregar tareas

--- tareas_app/urls.py
+++ tareas_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("listar/", views.listar_tareas),
+    path("listar/", views.listar_tareas, name="listar_tareas"),
+    path("agregar/", views.agregar_tarea, name="agregar_tarea"),
 ]
--- tareas_app/views.py
+++ tareas_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Tarea
 
 # Create your views here.
@@ -9,3 +9,25 @@ def listar_tareas(request):
 
     # Pasar como parametro para que pueda mostrar todo
     return render(request, 'listar_tareas.html', {'tareas': tareas})
+
+def agregar_tarea(request):
+    # Si se presiona enviar en el formulario, hace esto
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        desc = request.POST.get('desc')
+
+        if request.POST.get('completada') == 'on':
+            completada = True
+        else:
+            completada = False
+
+        Tarea.objects.create(
+            nombre = nombre,
+            desc = desc,
+            completada = completada
+        )
+
+        return redirect('agregar_tarea')
+
+    # Sino, solo carga la pagina
+    return render(request, 'agregar_tarea.html')
